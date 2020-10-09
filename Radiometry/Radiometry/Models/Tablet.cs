053aff2e5e50a81b41787b44e277dbb87f816f43
zzhukanton <zzhukanton@gmail.com>
2020-10-10 00:40:09 +0300

    Add SQLite to Xamarin to store tablet list; add empty project for authorization module

---
+++ Radiometry/Radiometry/Models/Tablet.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+
+namespace Radiometry.Models
+{
+	public class Tablet
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public int XCount { get; set; }
+
+		public int YCount { get; set; }
+
+		public int XDelta { get; set; }
+
+		public int YDelta { get; set; }
+
+		public bool IsCompleted { get; set; }
+
+		public DateTime DateCompleted { get; set; }
+
+		public decimal Latitude { get; set; }
+
+		public decimal Longitude { get; set; }
+	}
+}
