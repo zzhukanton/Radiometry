8a1d7c881f873711ae5dbd3e7533d5cd8082ca6c
zzhukanton <zzhukanton@gmail.com>
2020-10-10 01:08:17 +0300

    add poc for fileService

---
+++ Authorization/Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Services
+{
+	public class FileService : IFileService
+	{
+		public string FileName => throw new NotImplementedException();
+
+
+
+		public Task<bool> DeleteMeasurement(int x, int y)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task EditMeasurement(int x, int y, int newValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> GetMeasurement(int x, int y)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> SaveMeasurement(int x, int y, int value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Authorization/Services/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Authorization.Services
+{
+	public interface IFileService
+	{
+		string FileName { get; }
+
+		Task<int> GetMeasurement(int x, int y);
+
+		Task<bool> SaveMeasurement(int x, int y, int value);
+
+		Task EditMeasurement(int x, int y, int newValue);
+
+		Task<bool> DeleteMeasurement(int x, int y);
+	}
+}
