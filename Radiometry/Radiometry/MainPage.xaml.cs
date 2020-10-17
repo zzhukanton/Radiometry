005f67bdc26a4f0c5bc364bc55835ea783f45d7a
zzhukanton <zzhukanton@gmail.com>
2020-10-17 20:52:39 +0300

    Add some styling to main page; experiments with carousel

--- Radiometry/Radiometry/MainPage.xaml.cs
+++ Radiometry/Radiometry/MainPage.xaml.cs
@@ -10,9 +10,27 @@ namespace Radiometry
 {
 	public partial class MainPage : ContentPage
 	{
+		public List<WelcomeItem> WelcomeItems { get; set; }
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+			this.WelcomeItems = new List<WelcomeItem>
+			{
+				new WelcomeItem { ImageUrl = "first.jpg", Message = "Начните работу прямо сейчас." },
+				new WelcomeItem { ImageUrl = "first.jpg", Message = "Используйте возможности мобильных устройств." },
+				new WelcomeItem { ImageUrl = "first.jpg", Message = "Узнать больше о радиометрии." }
+			};
+			this.BindingContext = this;
 		}
 	}
+
+	public class WelcomeItem 
+	{
+		public string ImageUrl { get; set; }
+
+		public string Message { get; set; }
+	}
+
 }
