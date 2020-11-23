c00b7c8eff2b9ae2dc8f26b2d72a8dfdb5651b2c
zzhukanton <zzhukanton@gmail.com>
2020-11-23 23:48:38 +0300

    fixes for anomaly

--- Radiometry/Radiometry/Tablets/Anomaly.xaml.cs
+++ Radiometry/Radiometry/Tablets/Anomaly.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Radiometry
@@ -13,6 +8,11 @@ namespace Radiometry
 		public Anomaly()
 		{
 			InitializeComponent();
+		}
+
+		private async void BackButton_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PopAsync();
 		}
 	}
 }
