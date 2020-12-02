6ef36cd2021a32afb9c65118f43d1424f036250f
zzhukanton <zzhukanton@gmail.com>
2020-12-03 01:59:30 +0300

    add file worker in clientl add token and user controllers; add cosmos db repository; add tablet processor project

---
+++ Radiometry.Authorization/Services/ITokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Radiometry.Authorization.Services
+{
+	public interface ITokenGenerator
+	{
+	}
+}
---
+++ Radiometry/Radiometry/FileService/IFileWorker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Radiometry.FileService
+{
+	public interface IFileWorker
+    {
+        Task<bool> ExistsAsync(string filename);
+
+        Task SaveTextAsync(string filename, string text);
+
+        Task<string> LoadTextAsync(string filename);
+
+        Task<IEnumerable<string>> GetFilesAsync();
+
+        Task DeleteAsync(string filename);
+    }
+}
